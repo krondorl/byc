c0b462ee0cb912412fd5fa5ee0a02df2945b1267
Adam Burucs <adam.burucs@gmail.com>
2026-01-02 12:24:16 +0100

    chore: delete args

--- src/cli/args.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 Adam Burucs. Licensed under custom Source Available License
-
-/// CLI argument definition
-///
-/// Hint:
-/// - Use clap derive
-pub fn parse_args() {
-    unimplemented!("CLI argument parsing")
-}
